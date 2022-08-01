89bb83e256347120383413e957dc6702f733bf9a
Gaurav Rajput <rajput.gaurav8341@gmail.com>
2022-08-01 16:35:25 +0530

    Paralaxiom REST API exercise

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+import random
 
 app = Flask(__name__)
 
@@ -6,6 +7,18 @@ app = Flask(__name__)
 def index():
     # return 'Hello World!!!'
     return render_template('index.html')
+
+
+@app.route('/rand_string', methods = [ 'GET', 'POST'])
+def rand_string():
+    # return 'Hello World!!!'
+    if request.method == 'POST':
+        s = request.get_json(force = True)['random_string']
+        return jsonify({'random_string':s, 'random number': random.randint(0, 100)})
+    elif request.method == 'GET':
+        s = request.args.get('random_string')
+        return jsonify({'random_string':s, 'random number': random.randint(0, 100)})
+        
 
 if __name__ == '__main__':
     app.run(debug = True)
